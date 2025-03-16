fb0fc9c1ff55de6112c9f78066de5fabf7459046
Stanchevvv <122915555+Stanchevvv@users.noreply.github.com>
2025-03-16 18:48:52 +0200

    Login and Register Components

--- expense-tracker/app/(auth)/register.tsx
+++ expense-tracker/app/(auth)/register.tsx
@@ -1,14 +1,116 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { useRef, useState } from 'react'
+import ScreenWrapper from '@/components/ScreenWrapper'
+import Typo from '@/components/Typo'
+import { colors, spacingX, spacingY } from '@/constants/theme'
+import { verticalScale } from '@/utils/styling'
+import BackButton from '@/components/BackButton'
+import Input from '@/components/Input'
+import * as Icons from "phosphor-react-native"
+import Button from '@/components/Button'
+import { useRouter } from 'expo-router'
 
 const Register = () => {
+
+    const emailRef = useRef("")
+    const passwordRef = useRef("")
+    const nameRef = useRef("")
+    const [isLoading, setIsLoading] = useState(false)
+    const router = useRouter()
+
+    const handleSubmit = async () => {
+        if(!emailRef.current || !passwordRef.current || !nameRef.current) {
+            console.log("emty fields");
+            Alert.alert('Sign up', 'Please fill all the fields')
+            return
+        }
+        console.log("email", emailRef.current);
+        console.log("password", nameRef.current);
+        console.log("password", passwordRef.current);
+        
+    }
+
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <ScreenWrapper>
+      <View style={styles.container}>
+         {/* back button */}
+         <BackButton iconSize={28} />
+         <View style={{gap: 5, marginTop: spacingY._20}}>
+            <Typo>Let's</Typo>
+            <Typo>get started</Typo>
+         </View>
+
+         {/* form */}
+         <View style={styles.form}>
+            <Typo size={16} color={colors.textLighter}>
+                Create an account to track your expenses
+            </Typo>
+            {/* Input */}
+            <Input 
+                placeholder='Enter your name'
+                onChangeText={(value) => (nameRef.current = value)}
+                icon={<Icons.User size={verticalScale(26)} color={colors.neutral300}  weight='fill'/>}
+            />
+
+            <Input 
+                placeholder='Enter your email'
+                onChangeText={(value) => (emailRef.current = value)}
+                icon={<Icons.At size={verticalScale(26)} color={colors.neutral300}  weight='fill'/>}
+            />
+
+            <Input 
+                placeholder='Enter your password'
+                secureTextEntry
+                onChangeText={(value) => (passwordRef.current = value)}
+                icon={<Icons.Lock size={verticalScale(26)} color={colors.neutral300}  weight='fill'/>}
+            />
+
+            <Button loading={isLoading} onPress={handleSubmit}>
+                <Typo fontWeight={"700"} color={colors.black} size={21}>Sign up</Typo>
+            </Button>
+         </View>
+         {/* footer */}
+         <View style={styles.footer}>
+            <Typo size={15}>Already have an account?</Typo>
+            <Pressable onPress={()=> router.navigate("/(auth)/login")}>
+                <Typo fontWeight={"700"} color={colors.primary} size={15}>Login</Typo>
+            </Pressable>
+         </View>
+      </View>
+    </ScreenWrapper>
   )
 }
 
 export default Register
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        gap: spacingY._30,
+        paddingHorizontal: spacingX._20
+    },
+    welcomeText: {
+        fontSize: verticalScale(20),
+        fontWeight: "bold",
+        color: colors.text,
+    },
+    form: {
+        gap: spacingY._20
+    },
+    forgotPassword: {
+        textAlign: "right",
+        fontWeight: "500",
+        color: colors.text,
+    },
+    footer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 5
+    },
+    footerText: {
+        textAlign: "center",
+        color: colors.text,
+        fontSize: verticalScale(15)
+    }
+})
