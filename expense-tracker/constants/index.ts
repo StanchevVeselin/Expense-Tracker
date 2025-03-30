50eed5977c527692786063b91326fd41c9bb8c10
Stanchevvv <122915555+Stanchevvv@users.noreply.github.com>
2025-03-30 13:07:37 +0300

    Cloudinary and upload image

---
+++ expense-tracker/constants/index.ts
@@ -0,0 +1,2 @@
+export const CLOUDINARY_CLOUD_NAME = "dqo7puknq";
+export const CLOUDINARY_UPLOAD_RESET = "images";
--- expense-tracker/services/userService.ts
+++ expense-tracker/services/userService.ts
@@ -1,13 +1,22 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, UserDataType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFileToCloudinary } from "./imageServise";
 
 
 export const updateUser = async (uid: string, updateData: UserDataType): Promise<ResponseType> => {
     try{
+
+        if(updateData.image && updateData?.image?.uri) {
+            const imageUploadRes = await uploadFileToCloudinary(updateData.image, "users");
+            if(!imageUploadRes.success) {
+                return {success: false, msg: imageUploadRes.msg || "Failed to upload image"};
+            }
+            updateData.image = imageUploadRes.data;
+        }
         const userRef = doc(firestore,"users", uid);
         await updateDoc(userRef, updateData);
-        return {success: true, msg: "updated successfully"}
+        return {success: true, msg: "updated successfully"};
     } catch(error: any){
         console.log("updated user error",error);
         return {success: false, msg: error?.message}
